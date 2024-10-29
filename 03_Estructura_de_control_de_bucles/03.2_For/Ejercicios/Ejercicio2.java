0894ee246010b0eb760446f181e70dbed587ec09
JuanjoRivero <juanjoriverolorido@iescastelar.com>
2024-10-29 16:05:23 +0100

    Add files via upload

---
+++ 03_Estructura_de_control_de_bucles/03.2_For/Ejercicios/Ejercicio2.java
@@ -0,0 +1,11 @@
+/*
+Ejercicio 2: Imprimir Números
+
+Enunciado: Completa el bucle for para imprimir los números del 1 al 5.
+*/
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        // Completa el bucle for aquí
+            // Completa aquí para imprimir el número
+    }
+}
