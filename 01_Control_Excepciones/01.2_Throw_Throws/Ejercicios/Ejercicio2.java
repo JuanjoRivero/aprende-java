8bcf53638d71b4aeb0516aec4386dead2f4726e5
JuanjoRivero <juanjoriverolorido@iescastelar.com>
2024-10-29 14:43:05 +0100

    Update Ejercicio2.java

--- 01_Control_Excepciones/01.2_Throw_Throws/Ejercicios/Ejercicio2.java
+++ 01_Control_Excepciones/01.2_Throw_Throws/Ejercicios/Ejercicio2.java
@@ -1,4 +1,3 @@
-package org.example;
 /*
 Ejercicio 2: Uso de throws en Método
 
@@ -9,16 +8,16 @@ Captura esta excepción y muestra "Error: División por cero no permitida."
 public class Ejercicio2 {
     public static void main(String[] args) {
         try {
-            dividir(59, 0); // Provoca ArithmeticException
-        } catch (ArithmeticException e) { // RELLENA EL PARENTESIS
+            dividir(59, 0); // -> Provoca ArithmeticException
+        } catch () { // RELLENA EL PARENTESIS
             System.out.println("Error: " + e.getMessage())
         }
         System.out.println("La ejecución continúa");
     }
 
-    public static void dividir(int a, int b) throws ArithmeticException{ // Añade un thows basado en ArithmeticException
+    public static void dividir(int a, int b) { // Añade un thows basado en ArithmeticException
         if (b == 0) {
-            throw new ArithmeticException ("Esto es un error");// Completa aquí para lanzar la excepción
+            // Completa aquí un thow para lanzar la excepción
         }
         int resultado = a / b;
         System.out.println("Resultado: " + resultado);
