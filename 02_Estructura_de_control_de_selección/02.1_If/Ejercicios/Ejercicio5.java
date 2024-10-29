de025cb8c688fcd92a80d3ef2ded667c34391156
JuanjoRivero <juanjoriverolorido@iescastelar.com>
2024-10-29 23:41:10 +0100

    Update Ejercicio5.java

--- 02_Estructura_de_control_de_selección/02.1_If/Ejercicios/Ejercicio5.java
+++ 02_Estructura_de_control_de_selección/02.1_If/Ejercicios/Ejercicio5.java
@@ -4,7 +4,6 @@ Ejercicio 5: Evaluación de Nota
 Enunciado: Completa el bloque `if-else if` para asignar una calificación basada en una puntuación. 
 Si la puntuación es mayor o igual a 90, imprime "A". Si es entre 80 y 89, imprime "B". 
 Si es entre 70 y 79, imprime "C". Si es menor de 70, imprime "F".
-NOTA: && sirve como and, y te ayudará a realizar en una condicion de 2 condiciones (EJEMPLO: puntuacion >= 80 && puntuacion <= 89)
 */
 public class Ejercicio5 {
     public static void main(String[] args) {
