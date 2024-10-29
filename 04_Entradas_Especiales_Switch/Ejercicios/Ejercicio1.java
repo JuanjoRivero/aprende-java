9a435867239a7621b2c66ef0c557d7ffd113c620
JuanjoRivero <juanjoriverolorido@iescastelar.com>
2024-10-29 16:24:31 +0100

    Add files via upload

---
+++ 04_Entradas_Especiales_Switch/Ejercicios/Ejercicio1.java
@@ -0,0 +1,19 @@
+/*
+Ejercicio 1: Sumar Números
+
+Enunciado: Completa el programa para que, al ingresar un número, 
+imprima la suma de los números del 1 al número ingresado. No puede 
+ser superior a 10 este numero. Si el usuario ingresa -a o --all, 
+el programa debe imprimir la suma de los números del 1 al 10.
+*/
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Ingresa un número o -a/--all: ");
+        String input = teclado.nextLine(); // Leer entrada del usuario
+
+        // Completa aquí
+    }
+}
