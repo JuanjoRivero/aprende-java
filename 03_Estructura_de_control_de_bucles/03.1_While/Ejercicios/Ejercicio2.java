c8b123167dce4cbce4ba6209d1e2f4ff35ae4369
JuanjoRivero <juanjoriverolorido@iescastelar.com>
2024-10-30 01:58:43 +0100

    Update Ejercicio2.java

--- 03_Estructura_de_control_de_bucles/03.1_While/Ejercicios/Ejercicio2.java
+++ 03_Estructura_de_control_de_bucles/03.1_While/Ejercicios/Ejercicio2.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Ejercicio2 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int numero = ; // Ingresa numero mediante Scanner
+        int numero = 0; 
         int suma = 0;
 
         // Completa aquí para solicitar números al usuario hasta que ingrese un número negativo
